0057119c1ae013e6351058ef22e7ab23203dd4a8
YAMAGAMI Satoshi <s.yamagami.666@gmail.com>
2025-08-11 13:06:18 +0900

    Create stub implementation for optimized component store

---
+++ internal/core/ecs/optimizations/cache/optimized_component_store.go
@@ -0,0 +1,52 @@
+package cache
+
+import (
+	"github.com/muscle-dreamer/internal/core/ecs"
+)
+
+// OptimizedComponentStore は CPU キャッシュ効率を最適化したコンポーネントストア
+type OptimizedComponentStore struct {
+	// 現時点では空の構造体（テストがコンパイルできる最小限）
+}
+
+// NewOptimizedComponentStore creates a new optimized component store
+func NewOptimizedComponentStore() *OptimizedComponentStore {
+	return &OptimizedComponentStore{}
+}
+
+// AddTransform adds a transform component (stub implementation)
+func (cs *OptimizedComponentStore) AddTransform(entityID ecs.EntityID, component ecs.TransformComponent) {
+	// TODO: 実装予定
+}
+
+// GetTransform gets a transform component (stub implementation)
+func (cs *OptimizedComponentStore) GetTransform(entityID ecs.EntityID) *ecs.TransformComponent {
+	// TODO: 実装予定
+	return nil
+}
+
+// GetTransformArray returns the transform array for SoA access (stub)
+func (cs *OptimizedComponentStore) GetTransformArray() []ecs.TransformComponent {
+	// TODO: 実装予定
+	return nil
+}
+
+// PrefetchComponents prefetches components for better cache performance (stub)
+func (cs *OptimizedComponentStore) PrefetchComponents(entities []ecs.EntityID) {
+	// TODO: 実装予定
+}
+
+// RemoveTransform removes a transform component (stub)
+func (cs *OptimizedComponentStore) RemoveTransform(entityID ecs.EntityID) {
+	// TODO: 実装予定
+}
+
+// AddSprite adds a sprite component (stub)
+func (cs *OptimizedComponentStore) AddSprite(entityID ecs.EntityID, component ecs.SpriteComponent) {
+	// TODO: 実装予定
+}
+
+// RemoveSprite removes a sprite component (stub)
+func (cs *OptimizedComponentStore) RemoveSprite(entityID ecs.EntityID) {
+	// TODO: 実装予定
+}
